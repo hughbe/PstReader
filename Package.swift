f8f2c28ed6dd29ddc4e1872d506be04236ca5636
Hugh Bellamy <hughbellars@gmail.com>
2020-10-30 14:38:06 +0000

    Commit horrible file

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/hughbe/DataStream", from: "1.1.1"),
         .package(url: "https://github.com/hughbe/WindowsDataTypes", from: "1.0.0"),
-        .package(name: "MAPI", path: "../SwiftMAPI")
+        .package(name: "MAPI", url: "https://github.com/hughbe/SwiftMAPI", from: "1.0.0")
     ],
     targets: [
         .target(
